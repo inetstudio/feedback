277340454867d355149bd10678d569904886dcd6
Konstantin Bukin <bukin@inetstudio.ru>
2018-08-25 03:48:09 +0300

    Turn off observer

--- src/Providers/FeedbackServiceProvider.php
+++ src/Providers/FeedbackServiceProvider.php
@@ -28,7 +28,7 @@ class FeedbackServiceProvider extends ServiceProvider
         $this->registerViews();
         $this->registerTranslations();
         $this->registerEvents();
-        $this->registerObservers();
+        //$this->registerObservers();
         $this->registerViewComposers();
     }
 
