c4266db61e00b31427b80eb81ec3acbe141b1a9f
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-15 04:05:51 +0300

    Fix captcha validation

--- src/Http/Controllers/Front/FeedbackController.php
+++ src/Http/Controllers/Front/FeedbackController.php
@@ -2,9 +2,11 @@
 
 namespace InetStudio\Feedback\Http\Controllers\Front;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Arcanedev\NoCaptcha\Rules\CaptchaRule;
 use InetStudio\Feedback\Contracts\Services\Front\FeedbackServiceContract;
-use InetStudio\Feedback\Contracts\Http\Requests\Front\SendFeedbackRequestContract;
 use InetStudio\Feedback\Contracts\Http\Responses\Front\SendFeedbackResponseContract;
 
 /**
@@ -16,12 +18,38 @@ class FeedbackController extends Controller
      * Отправляем сообщение с формы обратной связи.
      *
      * @param FeedbackServiceContract $feedbackService
-     * @param SendFeedbackRequestContract $request
+     * @param Request $request
      *
      * @return SendFeedbackResponseContract
      */
-    public function sendFeedback(FeedbackServiceContract $feedbackService, SendFeedbackRequestContract $request): SendFeedbackResponseContract
+    public function sendFeedback(FeedbackServiceContract $feedbackService, Request $request): SendFeedbackResponseContract
     {
+        $rules = [
+            'message' => 'required',
+        ];
+
+        if (! auth()->user()) {
+            $rules = array_merge($rules, [
+                'name' => 'required|max:255',
+                'email' => 'required|max:255|email',
+                'g-recaptcha-response' => [
+                    'required',
+                    new CaptchaRule,
+                ],
+            ]);
+        }
+
+        Validator::make($request->all(), $rules, [
+            'message.required' => 'Поле «Сообщение» обязательно для заполнения',
+            'name.required' => 'Поле «Имя» обязательно для заполнения',
+            'name.max' => 'Поле «Имя» не должно превышать 255 символов',
+            'email.required' => 'Поле «Email» обязательно для заполнения',
+            'email.max' => 'Поле «Email» не должно превышать 255 символов',
+            'email.email' => 'Поле «Email» должно содержать значение в корректном формате',
+            'g-recaptcha-response.required' => 'Поле «Капча» обязательно для заполнения',
+            'g-recaptcha-response.captcha'  => 'Неверный код капча',
+        ])->validate();
+
         $data = $request->only($feedbackService->model->getFillable());
 
         $item = $feedbackService->sendFeedback($data);
