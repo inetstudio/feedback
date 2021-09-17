48ade794fd9ed789ddf07f9f2132cebdc038f8d1
Konstantin Bukin <bukin@inetstudio.ru>
2021-09-17 23:51:44 +0300

    Update menu

--- entities/feedback/resources/views/back/includes/navigation.blade.php
+++ entities/feedback/resources/views/back/includes/navigation.blade.php
@@ -4,6 +4,6 @@
     $unreadBadge = $feedbackService->getUnreadItemsCount();
 @endphp
 
-<li class="{{ isActiveRoute('back.feedback.*') }}">
+<li class="{{ isActiveRoute('back.feedback.*', 'mm-active') }}">
     <a href="{{ route('back.feedback.index') }}"><i class="fa fa-envelope"></i> <span class="nav-label">Обратная связь</span><span class="label label-primary float-right">{{ $unreadBadge }}</span></a>
 </li>
